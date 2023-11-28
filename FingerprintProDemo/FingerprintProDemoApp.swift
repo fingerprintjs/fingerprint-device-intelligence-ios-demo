9656711964435d98ac61ac1536de7d3566ed4c0b
Michał Gutowski <mgutski@icloud.com>
2023-11-28 12:26:06 +0100

    Add reusable FingerprintButtonStyle

--- FingerprintProDemo/FingerprintProDemoApp.swift
+++ FingerprintProDemo/FingerprintProDemoApp.swift
@@ -5,7 +5,17 @@ struct FingerprintProDemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            EmptyView()
+            VStack {
+                Spacer()
+                Button(
+                    action: {},
+                    label: {
+                        Image("FingerprintButtonImage")
+                    }
+                )
+                .buttonStyle(.fingerprint)
+                .padding(.bottom, 96.0)
+            }
         }
     }
 }
---
+++ FingerprintProDemo/UI/Styles/FingerprintButtonStyle.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+extension ButtonStyle where Self == FingerprintButtonStyle {
+
+    static var fingerprint: Self { .init() }
+}
+
+struct FingerprintButtonStyle: ButtonStyle {
+
+    @Environment(\.isEnabled) private var isEnabled: Bool
+
+    @State private var isAnimating: Bool = false
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.all, 28.0)
+            .background(background(for: configuration))
+            .clipShape(Circle())
+            .scaleEffect(scaleFactor(for: configuration))
+            .animation(scaleAnimation, value: isAnimating)
+            .animation(pressedAnimation, value: configuration.isPressed)
+            .onAppear {
+                isAnimating = isEnabled
+            }
+            .onChange(of: isEnabled) { value in
+                isAnimating = value
+            }
+            .onTapGesture {
+                isAnimating = false
+            }
+    }
+}
+
+private extension FingerprintButtonStyle {
+
+    func background(for configuration: Configuration) -> some ShapeStyle {
+        guard isEnabled else {
+            return .lightOrange
+        }
+
+        return configuration.isPressed ? .darkOrange : .accent
+    }
+
+    func scaleFactor(for configuration: Configuration) -> CGFloat {
+        guard !configuration.isPressed else {
+            return 0.9
+        }
+
+        return isAnimating ? 1.1 : 1.0
+    }
+}
+
+private extension FingerprintButtonStyle {
+
+    var scaleAnimation: Animation {
+        guard isAnimating else {
+            return .linear
+        }
+
+        return Animation
+            .easeInOut(duration: 0.6)
+            .repeatForever()
+    }
+
+    var pressedAnimation: Animation {
+        .easeOut(duration: 0.25)
+    }
+}
