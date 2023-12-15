2418a9075d1f7ba4d8fc7ac8799471d26b784ff3
Michał Gutowski <mgutski@icloud.com>
2023-12-15 13:33:53 +0100

    Resolve API key based on active build configuration
    Employs Swift Confidential (a third-party dependency) for obfuscating the
    API key for release builds, as defined in `confidential.yml`
    configuration file. The configuration file is intentionally excluded
    from source control.

---
+++ FingerprintProDemo/Config/ConfigVariable.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+enum ConfigVariable {
+
+    #if DEBUG
+    private static let apiKeyEnvVarName = "API_KEY"
+    #endif
+
+    static var apiKey: String {
+        #if DEBUG
+        guard let key = ProcessInfo.processInfo.environment[apiKeyEnvVarName] else {
+            let errorMessage = """
+                Could not resolve API key. \
+                Please set \(apiKeyEnvVarName) environment variable in active scheme or test plan.
+                """
+            fatalError(errorMessage)
+        }
+
+        return key
+        #else
+        ObfuscatedLiterals.$apiKey
+        #endif
+    }
+}
