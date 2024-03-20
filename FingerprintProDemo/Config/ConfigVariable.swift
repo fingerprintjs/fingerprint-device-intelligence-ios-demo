6e5904936b15d27b3c2e57dd945d8f42f3cd417a
Michał Gutowski <mgutski@icloud.com>
2024-03-20 15:23:30 +0100

    Simplify app configuration to improve DX

--- FingerprintProDemo/Config/ConfigVariable.swift
+++ FingerprintProDemo/Config/ConfigVariable.swift
@@ -4,11 +4,18 @@ import Foundation
 enum ConfigVariable {
 
     #if DEBUG
+    private enum Developer {
+        static let apiKey: String? = .none  // <- Set your Public API Key here
+        static let region: Region? = .none  // <- Set your Region here
+    }
+
     private static let apiKeyEnvVarName = "API_KEY"
     #endif
 
     static var apiKey: String {
         #if DEBUG
+        if let key = Developer.apiKey { return key }
+
         guard let key = ProcessInfo.processInfo.environment[apiKeyEnvVarName] else {
             let errorMessage = """
                 Could not resolve API key. \
@@ -25,7 +32,7 @@ enum ConfigVariable {
 
     static var region: Region {
         #if DEBUG
-        .global
+        Developer.region ?? .global
         #else
         .ap
         #endif
