9b799c4854ad389e8e22c8079e1c3ad99a4cb952
Michał Gutowski <mgutski@icloud.com>
2024-04-12 11:20:59 +0200

    Resolve Sign Up URL based on active build configuration

--- FingerprintProDemo/Constants/Constants.swift
+++ FingerprintProDemo/Constants/Constants.swift
@@ -5,7 +5,14 @@ enum C {
     enum URLs {
         static var documentation: URL { .init(staticString: "https://dev.fingerprint.com/docs/ios-sdk") }
         static var privacyPolicy: URL { .init(staticString: "https://dev.fingerprint.com/docs/privacy-policy") }
-        static var signUp: URL { .init(staticString: "https://dashboard.fingerprint.com/signup") }
+        static var signUp: URL {
+            #if DEBUG
+            .init(staticString: "https://dashboard.fingerprint.com/signup")
+            #else
+            guard let url = URL(string: ObfuscatedLiterals.$signUpURLString) else { preconditionFailure() }
+            return url
+            #endif
+        }
         static var support: URL { .init(staticString: "https://fingerprint.com/support") }
     }
 }
