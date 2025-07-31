88b57b752a43c04bcd82d6d853020b36054e3d23
Mikita Smalianchanka <mikita.smalianchanka@fingerprint.com>
2025-07-31 19:12:59 +0100

    Fix JSON escaping in raw response

--- FingerprintProDemo/Features/Home/ViewModel/ClientResponseEventViewModel.swift
+++ FingerprintProDemo/Features/Home/ViewModel/ClientResponseEventViewModel.swift
@@ -28,7 +28,7 @@ extension ClientResponseEventViewModel {
     var rawEventRepresentation: String {
         let encoder = JSONEncoder()
         encoder.dateEncodingStrategy = .iso8601Full
-        encoder.outputFormatting = [.prettyPrinted, .sortedKeys]
+        encoder.outputFormatting = [.prettyPrinted, .sortedKeys, .withoutEscapingSlashes]
 
         guard
             let jsonData = try? encoder.encode(event),
@@ -139,7 +139,11 @@ private extension ClientResponseEventViewModel {
         guard vpn.data.result else {
             return LocalizedStrings.notDetected.rawValue
         }
-        return .init(localized: "Device time zone is \(vpn.data.originTimezone)")
+        if vpn.data.methods.auxiliaryMobile {
+            return .init(localized: "Device has VPN enabled")
+        } else {
+            return .init(localized: "VPN usage suspected, device timezone is \(vpn.data.originTimezone)")
+        }
     }
 
     var factoryResetItemValue: AttributedString {
