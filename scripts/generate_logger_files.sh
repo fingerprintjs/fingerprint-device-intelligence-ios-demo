d7bc00a09012ee1dfbb436c3e705e4f36ddb86e5
Rachel Polachova <rachel.polachova@fingerprint.com>
2026-03-20 09:49:55 +0100

    Symlink logger frameworks

--- scripts/generate_logger_files.sh
+++ scripts/generate_logger_files.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Only run for Release builds
-[[ "${CONFIGURATION:-}" == "Release" ]] || exit 0
+FOLDER="${PROJECT_DIR}/FingerprintProDemo/Generated"
+FILE="${FOLDER}/Confidential.generated.swift"
 
-if ! command -v swift-logger >/dev/null 2>&1; then
-    echo "ERROR: swift-logger is not installed." >&2
-    exit 1
+if [[ "${CONFIGURATION:-}" == "Release" ]]; then
+    
+	if ! command -v swift-logger >/dev/null 2>&1; then
+		echo "ERROR: swift-logger is not installed." >&2
+		exit 1
+	fi
+
+	mkdir -p "$FOLDER"
+	swift-logger --configuration confidential.yml --output "$FILE"
+
+else
+    
+    if [[ -f "$FILE" ]]; then
+        echo "" > "$FILE"
+    fi
 fi
-
-swift-logger --configuration confidential.yml --output "${PROJECT_DIR}/FingerprintProDemo/Generated/Confidential.generated.swift"
